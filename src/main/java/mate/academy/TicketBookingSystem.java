e8a92f4f262693d5c17a05a6602184aeea7add46
Yevhen Shumeiko <shymeiko.zenya@gmail.com>
2024-11-05 00:15:54 +0200

    solved

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,30 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private final AtomicInteger totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats, true);
+        this.totalSeats = new AtomicInteger(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (totalSeats.intValue() > 0) {
+                totalSeats.decrementAndGet();
+                return new BookingResult(user, true, "Booking successful.");
+            }
+            return new BookingResult(user, false, "No seats available.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
